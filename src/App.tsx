dec6bf3012962cee9ad8a3f4068f1ad53f608c60
Juan Lopez <aragonlopezjuan87@gmail.com>
2023-02-13 13:29:09 +0100

    get data server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react'
 import { Nav, Localition, Weather, Forecast } from './components'
+import axios from 'axios'
+
+const getData = async () => {
+  const res = await axios('../server/index.json')
+  if (res.statusText !== 'OK') {
+    return Error("tenemos un error");
+
+  }
+  return res.data
+}
 
 function App() {
+  const [weather, setWeather] = useState([])
+
+  console.log(weather)
+
+  useEffect(() => {
+    getData()
+      .then(res => setWeather(res))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="bg-slate-700 h-screen text-cyan-50 px-4 ">
